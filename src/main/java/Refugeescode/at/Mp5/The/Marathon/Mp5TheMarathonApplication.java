a336e331dfcaeae7edc2fe0e26048de9757bfdd9
MohamedDahman <mhd.dahman@yahoo.com>
2018-03-27 08:24:41 +0200

    initialize Mp5 - The Marathon

---
+++ src/main/java/Refugeescode/at/Mp5/The/Marathon/Mp5TheMarathonApplication.java
@@ -0,0 +1,12 @@
+package Refugeescode.at.Mp5.The.Marathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp5TheMarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp5TheMarathonApplication.class, args);
+	}
+}
